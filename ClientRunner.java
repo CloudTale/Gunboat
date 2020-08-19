49451743cf5558f0ede056930c7d172230fa23f0
CloudTale <69694439+CloudTale@users.noreply.github.com>
2020-08-19 11:40:18 -0700

    Add files via upload

---
+++ ClientRunner.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ClientRunner
+{
+   public static void main(String[] args)
+   {
+   Scanner reader = new Scanner(System.in);
+   
+   System.out.println("Enter distance to target. \n");
+   double d = reader.nextDouble();
+   System.out.println("Enter height of battery relative to water line. \n");
+   double h = reader.nextDouble();
+   System.out.println("Enter initial velocity of shell. \n");
+   double v = reader.nextDouble();
+   
+   double a = -4.9*(d/v)*(d/v);
+   double b = d;
+   double c = h + a;
+   
+   double tanTheta = (-b + Math.sqrt(b*b - 4*a*c))/(2*a);
+   double angle = Math.atan(tanTheta)*180/3.14159;
+   System.out.println(angle + " degrees");
+   
+   
+   }
+}
